6d62efa7be7bee2d3614dba5e5f2005aad795501
Alexander <yusovsky2@gmail.com>
2023-12-17 18:12:48 +0200

    added profile page

--- mongoose/models/User.ts
+++ mongoose/models/User.ts
@@ -2,6 +2,7 @@ import { Schema, models, model } from "mongoose";
 
 const userSchema = new Schema(
   {
+    name: { type: String, required: false },
     email: { type: String, required: true, unique: true },
     password: {
       type: String,
--- sections/Footer/Footer.tsx
+++ sections/Footer/Footer.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import { authConfig } from "@/configs/auth";
+import { getServerSession } from "next-auth";
 
-const Footer = () => {
+const Footer = async () => {
+  const session = await getServerSession(authConfig);
   return (
-    <footer className="flex justify-center items-center border-[1px] border-orange-950 h-[60px]">
+    <footer className="flex flex-col justify-center items-center border-[1px] border-orange-950 py-[0px]">
       <h2>Footer</h2>
+      <p>this is serverside component for display username:</p>
+
+      {/* {попробовать перевести показ сессии на клиента} */}
+      <p>{session?.user?.name}</p>
     </footer>
   );
 };

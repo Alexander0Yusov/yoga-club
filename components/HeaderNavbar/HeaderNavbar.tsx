b39739868c6baf84e1250d5c02ddb222e5228c85
Alexander <yusovsky2@gmail.com>
2023-12-17 21:10:53 +0200

    header navbar become client component

---
+++ components/HeaderNavbar/HeaderNavbar.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { LocaleT } from "@/i18nConfig";
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import React from "react";
+
+const HeaderNavbar = ({ lang }: { lang: LocaleT }) => {
+  const session = useSession();
+
+  return (
+    <nav className="flex gap-3 border-[1px] border-orange-700 p-1">
+      <Link href={`/${lang}`}>Home</Link>
+      <Link href={`/${lang}/events`}>To events</Link>
+
+      {session.status === "authenticated" && (
+        <div className="flex gap-3 border-[1px] border-orange-950 p-[1px]">
+          <Link href={"/profile"} className=" border-[1px] border-orange-950 ">
+            Profile {session.data.user?.name?.split(" ")[0]}
+          </Link>
+          <button
+            className=" border-[1px] border-orange-950 "
+            onClick={() => signOut()}
+          >
+            Logout
+          </button>
+        </div>
+      )}
+      {session.status !== "authenticated" && (
+        <div className="flex gap-3 border-[1px] border-orange-950 ">
+          <Link href={`/${lang}/signin`}>Sign In</Link>
+          <Link href={`/${lang}/signup`}>Sign Up</Link>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default HeaderNavbar;
--- sections/Header/Header.tsx
+++ sections/Header/Header.tsx
@@ -1,62 +1,24 @@
-"use client";
 import initTranslations from "@/app/[lang]/i18n";
+import HeaderNavbar from "@/components/HeaderNavbar/HeaderNavbar";
 import LanguageChanger from "@/components/LanguageChanger/LanguageChanger";
 import Logo from "@/components/Logo/Logo";
 import { TranslationsProvider } from "@/components/TranslationsProvider";
-import { signOut, useSession } from "next-auth/react";
-import Link from "next/link";
-import { useEffect, useState } from "react";
 
 const i18nNamespaces = ["homePage"];
 
-const Header = ({ lang }: { lang: any }) => {
-  const [res, setRes] = useState({ t: null, resources: null });
-
-  useEffect(() => {
-    // желательно вернуть в статус серверного, это даст асинхр запрос. а сессию получить в клиентском подкомпоненте
-    //const { t, resources } =
-    initTranslations(lang, i18nNamespaces).then(
-      ({ t = null, resources = null }) => setRes({ t, resources } as any)
-    );
-  }, []);
-
-  const session = useSession();
+const Header = async ({ lang }: { lang: any }) => {
+  const { t, resources } = await initTranslations(lang, i18nNamespaces);
 
   return (
     <TranslationsProvider
       namespaces={i18nNamespaces}
       locale={lang}
-      resources={res?.resources}
+      resources={resources}
     >
       <header className="flex justify-between items-center border-[1px] border-orange-950 h-[60px]">
         <Logo />
-        <nav className="flex gap-3 border-[1px] border-orange-950 p-1">
-          <Link href={`/${lang}`}>Home</Link>
-          <Link href={`/${lang}/events`}>To events</Link>
 
-          {session.status === "authenticated" && (
-            <div className="flex gap-3 border-[1px] border-orange-950 p-[1px]">
-              <Link
-                href={"/profile"}
-                className=" border-[1px] border-orange-950 "
-              >
-                Profile {session.data.user?.name?.split(" ")[0]}
-              </Link>
-              <button
-                className=" border-[1px] border-orange-950 "
-                onClick={() => signOut()}
-              >
-                Logout
-              </button>
-            </div>
-          )}
-          {session.status === "unauthenticated" && (
-            <div className="flex gap-3 border-[1px] border-orange-950 ">
-              <Link href={`/${lang}/signin`}>Sign In</Link>
-              <Link href={`/${lang}/signup`}>Sign Up</Link>
-            </div>
-          )}
-        </nav>
+        <HeaderNavbar lang={lang} />
 
         <LanguageChanger />
       </header>

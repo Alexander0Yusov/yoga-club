dea2dd5321ca15583b5023e864bf8882608d4e24
Alexander0Yusov <yusovsky2@gmail.com>
2026-03-22 03:35:16 +0200

    chore: checkpoint final router validation

--- i18nConfig.ts
+++ i18nConfig.ts
@@ -3,6 +3,7 @@ import { Config } from "next-i18n-router/dist/types";
 export const i18nConfig: Config = {
   defaultLocale: "uk",
   locales: ["uk", "ru", "en", "de"],
+  prefixDefault: true,
 };
 
 export type LocaleT = (typeof i18nConfig)["locales"][number];

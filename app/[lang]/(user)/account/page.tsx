3d369a54832d8bd74795c3847c5477d7a6420b94
Alexander0Yusov <yusovsky2@gmail.com>
2026-03-22 03:27:52 +0200

    chore: checkpoint account routes return

---
+++ app/[lang]/(user)/account/page.tsx
@@ -0,0 +1,16 @@
+import { authConfig } from "@/configs/auth";
+import AccountDashboard from "@/features/account/ui/AccountDashboard";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+const AccountPage = async ({ params }: { params: { lang: string } }) => {
+  const session = await getServerSession(authConfig);
+
+  if (!session) {
+    redirect(`/${params.lang}/signin`);
+  }
+
+  return <AccountDashboard lang={params.lang} />;
+};
+
+export default AccountPage;
